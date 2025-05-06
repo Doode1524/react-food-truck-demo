2a5e7a65760d06e6229862e65a727adc32bcd061
“joey” <doode1524@gmail.com>
2025-05-05 22:23:30 -0400

    add sexy divider and improve about text

--- src/pages/LandingPage/components/LandingPageSection/index.tsx
+++ src/pages/LandingPage/components/LandingPageSection/index.tsx
@@ -4,13 +4,15 @@ import s from "./LandingPageSection.module.css";
 interface IProps {
   title: string;
   children: React.ReactNode;
+  hasDivider?: boolean;
 }
 
-const LandingPageSection = ({ children, title }: IProps) => {
+const LandingPageSection = ({ children, title, hasDivider = true }: IProps) => {
   return (
     <div className={s.sectionWrapper}>
       <h1 className={s.sectionTitle}>{title}</h1>
       {children}
+      {hasDivider && <div className={s.divider} />}
     </div>
   );
 };
